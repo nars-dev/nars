88414ad0b9571bfbe6e4f666d2490e9a632c7ffc
Wojtek Czekalski <me@wczekalski.com>
2019-09-09 15:01:21 +0200

    Fix github publish action It will publish the package if version changed in package.json

--- packages/nars-common/generate_proto.sh
+++ packages/nars-common/generate_proto.sh
@@ -4,6 +4,7 @@ yarn pbjs -t static-module -w commonjs -o ./dist/Schema.js ./src/proto/schema.pr
 yarn pbts --no-comments -o ./src/Schema.d.ts ./dist/Schema.js
 cp ./src/Schema.d.ts ./dist/Schema.d.ts
 
-if [[ $1 == "--watch" ]]; then
+if [ "$1" == "--watch" ]
+then
   yarn watch ./generate_proto.sh ./src/proto
 fi
